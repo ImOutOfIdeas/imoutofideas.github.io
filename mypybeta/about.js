cb33ff5fe3ed5fdde5b3c015d056edca8e2ab854
ImOutOfIdeas <ImOutOfIdeas@users.noreply.github.com>
2020-10-05 20:25:41 -0400

    Added Voice Recognition Coloring

--- mypybeta/about.js
+++ mypybeta/about.js
@@ -6,7 +6,7 @@ if (accent) {
     document.documentElement.style.setProperty("--accent-color", accent);
 }
 
-var accentColor = ["#F52F57", "#008BF8", "#35A7FF", "#98CE00", "#F5F749", "#5E239D", "#CCFF66", "#FCAB10", "#E03616", "#C04CFD", "#AF42AE", "#F71735", "#41EAD4", "#03CEA4", "#FCAB10", "#F56476", "#008DD5", "#5EEB5B", "#C52184", "#50FFB1", "#01BAEF", "#FFB30F", "#FEEA00", "#70D6FF", "#4EFFEF", "#2CEAA3", "#EEC643", "#EEF0F2", "#E98A15", "#98E2C6", "#EF798A", "#7A28CB", "#FC60A8", "#FFE74C", "#35A7FF"];
+var accentColor = ["#F71735", "#008BF8", "#F5F749", "#C04CFD", "#FFAC0D", "#41EAD4", "#C52184", "#50FFB1", "#1EDA1B"];
 var oldColor = "";
 console.log(accentColor.length);
 
@@ -29,3 +29,75 @@ function themeSwitch() {
 switcher.onclick = () => {
     localStorage.setItem("--accent-color", newColor);
 }
+
+
+
+
+// Voice Recognition Color Changer
+
+const btn = document.querySelector("#speechRegBtn");
+// const content = document.querySelector(".content");
+const status = document.querySelector("#vrThemeSwither");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var colors = [ "red", "blue", "yellow", "purple", "Orange", "turquoise", "pink", "mint", "green"];
+var accentColor = ["#F71735", "#008BF8", "#F5F749", "#C04CFD", "#FFAC0D", "#41EAD4", "#C52184", "#50FFB1", "#1EDA1B"];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;';
+
+console.log("Voice commands, as of now, are not supported on MS Edge.")
+console.log("")
+console.log("List of available colors: " + colors)
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+function reset() {
+    status.innerHTML = "Voice Accent Color";
+}
+
+recognition.onstart = function() {
+    status.innerHTML = "Listening...";
+};
+
+recognition.onspeechend = function() {
+    recognition.stop();
+    status.innerHTML = "Stopped Listening";
+    setTimeout (reset, 800);
+};
+
+recognition.onnomatch = function(event) {
+    console.log("I didn't recognise that color.");
+    setTimeout (reset, 800);
+}
+
+recognition.onresult = function(event) {
+    var transcript = event.results[0][0].transcript;
+    var confidence = event.results[0][0].confidence;
+    console.log(transcript.toLowerCase() + ": " + confidence * 100 + "% Confidence");
+    if (colors.includes(transcript)){
+        console.log("Valid Color");
+        i = colors.indexOf(transcript);
+        vrColor = accentColor[i];
+        localStorage.setItem("--accent-color", vrColor);
+        document.documentElement.style.setProperty("--accent-color", vrColor);
+    }
+
+    setTimeout (reset, 800);
+};
+
+recognition.onerror = function(event) {
+    console.log(event.error);
+};
+
+btn.addEventListener("click", () => {
+    recognition.start();
+});
--- mypybeta/script.js
+++ mypybeta/script.js
@@ -1,8 +1,9 @@
 const logo = document.querySelectorAll("#logo path");
 
+/*// Gives Svg Lengths for start effect
 for (var i = 0; i<logo.length; i++) {
     console.log(`"Letter ${i + 1} is ${logo[i].getTotalLength()}`);
-}
+}*/
 
 const accent = localStorage.getItem("--accent-color");
 const switcher = document.getElementById("accentSwitcher");
@@ -12,9 +13,9 @@ if (accent) {
     document.documentElement.style.setProperty("--accent-color", accent);
 }
 
-var accentColor = ["#F52F57", "#008BF8", "#35A7FF", "#98CE00", "#F5F749", "#5E239D", "#CCFF66", "#FCAB10", "#E03616", "#C04CFD", "#AF42AE", "#F71735", "#41EAD4", "#03CEA4", "#FCAB10", "#F56476", "#008DD5", "#5EEB5B", "#C52184", "#50FFB1", "#01BAEF", "#FFB30F", "#FEEA00", "#70D6FF", "#4EFFEF", "#2CEAA3", "#EEC643", "#EEF0F2", "#E98A15", "#98E2C6", "#EF798A", "#7A28CB", "#FC60A8", "#FFE74C", "#35A7FF"];
+var accentColor = ["#F71735", "#008BF8", "#F5F749", "#C04CFD", "#FCAB10", "#41EAD4", "#C52184", "#50FFB1", "#1EDA1B", "#FFFFFF"];
 var oldColor = "";
-console.log(accentColor.length);
+// console.log(accentColor.length);
 
 function randInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -35,3 +36,74 @@ function themeSwitch() {
 switcher.onclick = () => {
     localStorage.setItem("--accent-color", newColor);
 }
+
+
+
+// Voice Recognition Color Changer
+
+const btn = document.querySelector("#speechRegBtn");
+// const content = document.querySelector(".content");
+const status = document.querySelector("#vrThemeSwither");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+var colors = [ "red", "blue", "yellow", "purple", "Orange", "turquoise", "pink", "mint", "green"];
+var accentColor = ["#F71735", "#008BF8", "#F5F749", "#C04CFD", "#FFAC0D", "#41EAD4", "#C52184", "#50FFB1", "#1EDA1B"];
+var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;';
+
+console.log("Voice commands, as of now, are not supported on MS Edge.")
+console.log("")
+console.log("List of available colors: " + colors)
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+speechRecognitionList.addFromString(grammar, 1);
+recognition.grammars = speechRecognitionList;
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+function reset() {
+    status.innerHTML = "Voice Accent Color";
+}
+
+recognition.onstart = function() {
+    status.innerHTML = "Listening...";
+};
+
+recognition.onspeechend = function() {
+    recognition.stop();
+    status.innerHTML = "Stopped Listening";
+    setTimeout (reset, 800);
+};
+
+recognition.onnomatch = function(event) {
+    console.log("I didn't recognise that color.");
+    setTimeout (reset, 800);
+}
+
+recognition.onresult = function(event) {
+    var transcript = event.results[0][0].transcript;
+    var confidence = event.results[0][0].confidence;
+    console.log(transcript.toLowerCase() + ": " + confidence * 100 + "% Confidence");
+    if (colors.includes(transcript)){
+        console.log("Valid Color");
+        i = colors.indexOf(transcript);
+        vrColor = accentColor[i];
+        localStorage.setItem("--accent-color", vrColor);
+        document.documentElement.style.setProperty("--accent-color", vrColor);
+    }
+
+    setTimeout (reset, 800);
+};
+
+recognition.onerror = function(event) {
+    console.log(event.error);
+};
+
+btn.addEventListener("click", () => {
+    recognition.start();
+});
