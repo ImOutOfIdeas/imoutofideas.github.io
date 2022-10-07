c3bea86129046ffcd7c67061aa3d55dfe1ba6bf4
ImOutOfIdeas <jonathandoyle655321@gmail.com>
2022-10-07 03:22:48 -0400

    kind of functioning login

--- recipes/public/app.js
+++ recipes/public/app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
-import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { getFirestore, collection, getDocs, doc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged , signInWithEmailAndPassword, sendEmailVerification } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 // Add additional SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,44 +14,47 @@ const firebaseConfig = {
   measurementId: "G-HVFC02L0V2"
 };
 
+
+
 // Initialize Firebase and relevant services
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-// Detect Auth State
-onAuthStateChanged(auth, user => {
-    if (user) {
-        console.log("Logged In As: ", user.email);
-        login();
-    }
-    else {
-        console.log("Not Logged In.");
+
+onAuthStateChanged(auth, (user) => {
+    if (user && user.emailVerified) {
+        window.location.replace('http://localhost:3000/home.html');
+    } else {
+      // User is signed out
     }
 });
 
-// 
-document.getElementById("submit").addEventListener ("click", () => {
+
+// Runs when submit is clicked
+document.getElementById("submit").addEventListener("click", () => {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-
-    console.log(email, password);
-
     signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            // Signed in 
+    .then((userCredential) => {
+        // Signed in 
             const user = userCredential.user;
+            if (user.emailVerified) {
+                // TODO: Replace with Actual Home Page URL
+                window.location.replace('http://localhost:3000/home.html');
+            } 
+            else {
+                sendEmailVerification(auth.currentUser)
+                    .then(() => {
+                        document.getElementById("loginContainer").style.display = "none";
+                        document.getElementById("emailVer").style.display = "block";
+                    });
+            }
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            // ..
+            console.log(errorMessage);
+            document.getElementById("error").classList.add("visible");
+            document.getElementById("password").value = "";
         });
 });
-
-async function login() {
-    const querySnapshot = await getDocs(collection(db, "recipes"));
-    querySnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-    });
-}
---
+++ recipes/public/home.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+// Add additional SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlRn1nz8gfmn-005C8WsYYq6irpbCMpHY",
+  authDomain: "friendly-recipes-bfa.firebaseapp.com",
+  databaseURL: "https://friendly-recipes-bfa-default-rtdb.firebaseio.com",
+  projectId: "friendly-recipes-bfa",
+  storageBucket: "friendly-recipes-bfa.appspot.com",
+  messagingSenderId: "942028263489",
+  appId: "1:942028263489:web:4da5ffc9468f9af11ef6c0",
+  measurementId: "G-HVFC02L0V2"
+};
+
+// Initialize Firebase and relevant services
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log(user);
+    } else {
+        console.log("No one is signed in");
+    }
+});
+
+// Handles Database access
+async function showRecipes() {
+    document.getElementById("notLoggedIn");
+    const querySnapshot = await getDocs(collection(db, "recipes"));
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+    });
+}
+
+async function addRecipe(name, ingredients, directions) {
+    try {
+        const docRef = await addDoc(collection(db, "recipes"), {
+            ingredients: ingredients,
+            directions: directions
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+}
+
+
+// Allow User to log out
+document.getElementById("logout").addEventListener("click", () => {
+    signOut(auth).then(() => {
+        window.location.replace("http://localhost:3000")
+    }).catch((error) => {
+        console.log(error);
+    });
+});
