0868e9c497c61b79cc6a7e087b825db71a55a623
Jonathan Doyle <jonathandoyle655321@gmail.com>
2026-03-05 23:11:52 -0500

    removed old stuff

--- circlelife/game/main.js
+++
@@ -1,9 +0,0 @@
-/** @type {HTMLCanvasElement} */
-
-// Setup
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
--- store/src/app/Task.ts
+++
@@ -1,7 +0,0 @@
-export interface Task {
-    // ? makes field optional
-    id?: number,
-    text: string,
-    day: string,
-    reminder: boolean
-}
